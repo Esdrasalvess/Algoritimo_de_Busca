6ad37409f775cffc2224fb56dae39a4e7ef211af
Esdrás Alves dos Santos <esdrasalvesdossantos2002@gmail.com>
2025-09-06 17:21:36 -0300

    Organizando branch back-informado: backend na raiz

--- front-dijkstra/src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app'; // ⬅ use AppComponent
-import { config } from './app/app.config.server';
-
-const bootstrap = () => bootstrapApplication(AppComponent, config); // ⬅ também aqui
-
-export default bootstrap;
